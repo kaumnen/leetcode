db2c74a80b9237662ca344d922fe2a95c3520537
Aleksej Komnenović <57869668+komnen0v1c@users.noreply.github.com>
2020-06-05 21:37:59 +0200

    Create 528. Random Pick with Weight.py

---
+++ 528. Random Pick with Weight.py
@@ -0,0 +1,8 @@
+import random
+class Solution:
+
+    def __init__(self, w):
+        self.w = list(itertools.accumulate(w))
+
+    def pickIndex(self):
+        return bisect.bisect_left(self.w, random.randint(1, self.w[-1]))
